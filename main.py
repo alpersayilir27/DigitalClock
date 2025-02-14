e0747ccc5079b24177ea1425b79c60567ff62626
Alper Sayılır <malper.sayilir@std.hku.edu.tr>
2025-02-14 11:19:37 +0300

    Update main.py
    Foreground color changed from yellow to orange

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ def time():
     label.config(text=string)
     label.after(1000,time)
 
-label = Label(root, font=("ds-digital", 80), background= "black", foreground="yellow")
+label = Label(root, font=("ds-digital", 80), background= "black", foreground="orange")
 label.pack(anchor="center")
 time()
 
 mainloop()
+
